96a9242d069cef3d981a3ac4fae81468585e49ee
ShawnHillJ <shawnhillj@gmail.com>
2020-07-22 12:17:28 -0700

    making a merge conflict

---
+++ Assets/Scripts/SampleScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //This is a sample script to test branching!
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

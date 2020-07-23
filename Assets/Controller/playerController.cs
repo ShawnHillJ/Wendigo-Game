351b0e3c3d89d80c084175e6a1f180d65cd5c8a9
Carnegie-senpai <sawyer3nick@gmail.com>
2020-07-22 21:21:51 -0700

    Introducing new MVC method of development

---
+++ Assets/Controller/playerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : rootElement
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Movement(GameObject obj, float vertical, float horizontal)
+    {
+        obj.transform.SetPositionAndRotation(new Vector3(obj.transform.position.x+(0.1f*horizontal), obj.transform.position.y, obj.transform.position.z+(0.1f*vertical)), obj.transform.rotation);
+    }
+}
---
+++ Assets/root.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rootElement : MonoBehaviour
+{
+
+    public root get_root
+    {
+        get
+        {
+            return FindObjectOfType<root>(); 
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public class root : MonoBehaviour
+{
+    public rootModel model;
+    public rootView view;
+    public rootController controller;
+}
